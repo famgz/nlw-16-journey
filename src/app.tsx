f5344f620df03c83df2a0468d61396be83e36838
famgz <famgz@proton.me>
2024-07-09 20:57:06 -0300

    chore: initial setup

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <h1 className="text-6xl font-bold text-red-500">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
